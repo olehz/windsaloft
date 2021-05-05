e42bf99adac999f47730ee99b27701254599fc63
Oleh Zamkovyi <oleh.zam@gmail.com>
2021-05-05 21:10:41 +0300

    New version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='windsaloft',
-    version='0.0.1',
+    version='0.0.2',
     description='Convert U/V raster to streamlines (geojson)',
     long_description=long_description,
     long_description_content_type="text/markdown",
